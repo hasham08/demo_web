3b6bd6f55098f873c22b8e5b89a04e2208840607
hasham08 <141668424+hasham08@users.noreply.github.com>
2025-09-09 01:45:22 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Welcome to wscube tech")
+st.title("Rifaah CROP PVT LTD")
+
+#===Left Menu==== 
+st.sidebar.text("Menu")
+
+st.sidebar.button("Emplooye")
+
+st.sidebar.button("Supplier")
+
+
